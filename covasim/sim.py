6751ad2469c489ea3b577ea04ad8092dd875b055
Ishan Vyas <ishan.vyas@duke.edu>
2025-04-01 16:17:58 -0400

    hosp_max/icu_max and dynamic contacts

--- covasim/sim.py
+++ covasim/sim.py
@@ -575,9 +575,15 @@ class Sim(cvb.BaseSim):
         self.rescale() # Check if we need to rescale
         people   = self.people # Shorten this for later use
         people.update_states_pre(t=t) # Update the state of everyone and count the flows
-        contacts = people.update_contacts() # Compute new contacts
+        """
+        BELOW IS COMMENTED OUT FOR SIMPLIFIED
+        contacts = people.update_contacts() 
         hosp_max = people.count('severe')   > self['n_beds_hosp'] if self['n_beds_hosp'] is not None else False # Check for acute bed constraint
         icu_max  = people.count('critical') > self['n_beds_icu']  if self['n_beds_icu']  is not None else False # Check for ICU bed constraint
+        """
+        # keep parameters from above though
+        hosp_max = False
+        icu_max  = False
 
         # Randomly infect some people (imported infections)
         if self['n_imports']:
