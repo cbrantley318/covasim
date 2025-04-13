1372fd367d9a865b4f2abf79e83cb0b3dc05b135
Ishan Vyas <ishan.vyas@duke.edu>
2025-04-12 23:59:19 -0400

    test

--- ishan_test.py
+++ ishan_test.py
@@ -1,3 +1,12 @@
 import covasim as cv
-sim = cv.Sim()
+
+# Approach 1: Specify parameters when creating the Sim object
+pars = dict(
+    pop_size = 60000,    # Number of people in the simulation
+    n_days = 50,         # Number of days to simulate
+    pop_infected = 4,   # Number of initial infections
+)
+sim = cv.Sim(pars)
 sim.run()
+
+# note: best bet is to increase the number of days to as much as possible.
