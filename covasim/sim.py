e564a2068cad960482f10eff569106b77a52cfaa
Ishan Vyas <ishan.vyas@duke.edu>
2025-04-13 15:40:22 -0400

    test3

--- covasim/sim.py
+++ covasim/sim.py
@@ -577,8 +577,7 @@ class Sim(cvb.BaseSim):
         people   = self.people # Shorten this for later use
         people.update_states_pre(t=t) # Update the state of everyone and count the flows
         contacts = people.update_contacts()
-        """
-        BELOW IS COMMENTED OUT FOR SIMPLIFIED
+        
  
         hosp_max = people.count('severe')   > self['n_beds_hosp'] if self['n_beds_hosp'] is not None else False # Check for acute bed constraint
         icu_max  = people.count('critical') > self['n_beds_icu']  if self['n_beds_icu']  is not None else False # Check for ICU bed constraint
